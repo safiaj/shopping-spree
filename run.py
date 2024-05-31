bc7350d6d8f198b52dbe263a95baeff898809d97
Safia Jama <safiajama@icloud.com>
2024-05-31 16:58:34 +0000

    Insert APIs and check access to google sheet is correct

--- run.py
+++ run.py
@@ -1,3 +1,29 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+"""
+Taken from love sandwiches module -
+imports the entire gspread library so we can access all functions/classses 
+within it and google auth imports the credentials class 
+which is part of the service account function from google auth libary
+""" 
+import gspread
+from google.oauth2.service_account import Credentials 
+
+"""
+Lists the APIs the program should to access for the
+program to run 
+"""
+SCOPE = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive.file",
+    "https://www.googleapis.com/auth/drive"
+    ]
+
+CREDS = Credentials.from_service_account_file('creds.json')
+SCOPED_CREDS = CREDS.with_scopes(SCOPE)
+GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
+SHEET = GSPREAD_CLIENT.open('shopping-spree')
+
+start = SHEET.worksheet('start')
+
+data = start.get_all_values()
+
+print(data)
